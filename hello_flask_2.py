a4362c91dda71ac0334b5350fdda1ec82dc99ef9
astroshima <42306523+astroshima@users.noreply.github.com>
2019-07-30 15:58:17 +0200

    With user_id

---
+++ hello_flask_2.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello():
+	return 'Hello world from Flask!'
+
+@app.route('/user/<user_id>') 	#za URL putanju '/user/<user_id>' pozovi funkciju getUser i prosledi joj string 'user_id'
+def getUser(user_id):
+	return 'I am user: %s.' % user_id
+	
+app.run()
