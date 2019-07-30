55722fc3e4405701def05ca053838aa5468b14fd
astroshima <42306523+astroshima@users.noreply.github.com>
2019-07-30 16:15:30 +0200

    Update hello_flask.py

--- hello_flask.py
+++ hello_flask.py
@@ -1,9 +1,9 @@
-from flask import Flask		#iz modula flask uvezi klasu Flask
+from flask import Flask     # iz modula flask uvezi klasu Flask
 
-app=Flask(__name__)
-
-@app.route('/')
-def hello() -> str:
+app = Flask(__name__)       # napravi objekat tipa Flask i dodeli ga promenljivoj app
+                            # __name__ : vrednost imena trenutno aktivnog modula
+@app.route('/')             # dekorator funkcije; kada Flaskov veb server dobije zahtev sa URL putanjom '/', pozovi funkciju hello() 
+def hello() -> str:         # definiši funkciju hello(), funkcija vraća string.
 	return 'Hello world from Flask!'
 	
-app.run()
+app.run()                   # pokreni veb server (tj. veb aplikaciju)
