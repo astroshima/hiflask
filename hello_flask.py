3531bbc804f4343b6ec2e91ffce00f0a9ad09121
astroshima <42306523+astroshima@users.noreply.github.com>
2019-08-11 16:52:56 +0200

    Update hello_flask.py

--- hello_flask.py
+++ hello_flask.py
@@ -9,3 +9,11 @@ def hello() -> str:
 print('Naziv aktivnog modula je:', __name__)
 	
 app.run()
+
+"""
+Ova veb aplikacija se (u Linuxu) pokreće iz komandne linije pomoću:
+$ python3 hello_flask.py
+Zatim se u veb brauzeru zadaje adresa:
+http://127.0.0.1:5000 ili
+http://localhost:5000
+"""
