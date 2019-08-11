669b1fdd49314df02021f0bca43a5584e11df1d4
astroshima <42306523+astroshima@users.noreply.github.com>
2019-08-11 16:37:14 +0200

    Update hello_flask_1_1.py

--- hello_flask_1_1.py
+++ hello_flask_1_1.py
@@ -3,13 +3,13 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
-def hello() -> str:
-	return 'Hello world from Flask!'
+def hello():
+    return 'Hello world from Flask!'
 
 print('Naziv aktivnog modula je:', __name__)
 
 if __name__=='__main__':	# ako je ovaj program startovan komandom python3
-	app.run()				# pokreni server
+    app.run()				# pokreni server
 
 """
 Startovanje servera iz komandne linije:
