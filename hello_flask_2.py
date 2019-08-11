3aca22f863ec1955e3e2250fa2ab4e5c14e92ec4
astroshima <42306523+astroshima@users.noreply.github.com>
2019-08-11 16:35:59 +0200

    Update hello_flask_2.py

--- hello_flask_2.py
+++ hello_flask_2.py
@@ -4,11 +4,11 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'Hello world from Flask!'
+    return 'Hello world from Flask!'
 
 @app.route('/<user_id>')    #za URL putanju '/<user_id>' pozovi funkciju getUser i prosledi joj string 'user_id'
 def getUser(user_id):
-	return 'I am user: %s.' % user_id
+    return 'I am user: %s.' % user_id
 
 if __name__=='__main__':
-	app.run()
+    app.run()
