42a2f6fbed684ebd48e2b11882ae392ee15c2906
astroshima <42306523+astroshima@users.noreply.github.com>
2019-08-11 17:22:27 +0200

    Update hello_flask_comments.py

--- hello_flask_comments.py
+++ hello_flask_comments.py
@@ -5,5 +5,15 @@ app = Flask(__name__)       # napravi objekat tipa Flask i dodeli ga promenljivo
 @app.route('/')             # dekorator funkcije koji mapira URL putanju (zahteva) na serveru, na akciju kontrolera (tj. funkciju); kada Flaskov veb server dobije zahtev sa URL putanjom '/', pozovi funkciju hello() 
 def hello() -> str:         # definiši funkciju hello(), funkcija vraća string.
     return 'Hello world from Flask!'
+
+print('Naziv aktivnog modula je:', __name__)    # ispisuje: Naziv aktivnog modula je __main__ ; jer startujemo program komandom: $ python3 hello_flask.py
 	
 app.run()                   # pokreni veb server i veb aplikaciju
+
+"""
+Ova veb aplikacija se (u Linuxu) pokreće iz komandne linije pomoću:
+$ python3 hello_flask.py
+Zatim se u veb brauzeru zadaje adresa:
+http://127.0.0.1:5000 ili
+http://localhost:5000
+"""
