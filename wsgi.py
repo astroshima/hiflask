f3c1a5405f4e07d7b7e3b644049575832cb8d1c0
astroshima <42306523+astroshima@users.noreply.github.com>
2019-08-17 02:07:02 +0200

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello world from Flask!'
+
+print('Naziv aktivnog modula je:', __name__)
+
+"""
+Ova veb aplikacija se (u Linuxu) pokreće iz komandne linije pomoću:
+$ flask run
+Zatim se u veb brauzeru zadaje adresa:
+http://127.0.0.1:5000 ili
+http://localhost:5000
+"""
